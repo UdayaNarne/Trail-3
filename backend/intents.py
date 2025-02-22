a8e783b91ab5b726d854a42c3cc8da9123410367
UdayaNarne <udayanarne08@gmail.com>
2025-02-22 13:07:12 +0530

    Updated intents file

--- backend/intents.py
+++ backend/intents.py
@@ -1,5 +1,8 @@
 import json
+import os
 label_map={'APPSDC': 0, 'Achievements': 1, 'Admission Details': 2, 'Admissions': 3, 'Admissions Details': 4, 'Alumni': 5, 'Anti-Ragging': 6, 'CFII': 7, 'Certificates': 8, 'College Details': 9, 'Contact Information': 10, 'E-mobility': 11, 'Events': 12, 'Facilities': 13, 'Faculty': 14, 'IIC': 15, 'IQAC': 16, 'Internships': 17, 'Magazine': 18, 'NSS': 19, 'Placements': 20, 'Results': 21, 'Syllabus': 22, 'Transfer and Migration': 23}
+backend_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(backend_dir, "label_map.json")
 with open("label_map.json", "w") as f:
     json.dump(label_map, f, indent=4)
 
